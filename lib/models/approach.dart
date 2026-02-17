c82acc6add256ed2ea9d4575be0a40a343d4a334
Badrakova Valeria <lera_18a@mail.ru>
2026-02-17 19:44:38 +0300

    refactor: add approach model and update models(exercise, trainings)

---
+++ lib/models/approach.dart
@@ -0,0 +1,8 @@
+import 'package:training_note/models/exercise.dart';
+
+class Approach {
+  final Exercise excercise;
+  final int repeats;
+
+  Approach({required this.excercise, required this.repeats});
+}
---
+++ lib/models/exercise.dart
@@ -0,0 +1,13 @@
+class Exercise {
+  final String name;
+
+  Exercise({
+    required this.name,
+  });
+}
+
+final List<Exercise> excercisesStub = [
+  Exercise(name: "Отжимания"),
+  Exercise(name: "Подтягивания"),
+  Exercise(name: "Приседания"),
+];
