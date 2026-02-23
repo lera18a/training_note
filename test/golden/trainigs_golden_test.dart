76a32b51cd8a8c812f1cc704f152e0dd96093152
JorikFat <jorikfat@gmail.com>
2026-02-23 20:15:12 +0300

    add: trainig list golden test

---
+++ test/golden/trainigs_golden_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:training_note/main.dart';
+import 'package:training_note/main_page.dart';
+
+void main() {
+  testWidgets('list', (tester) async {
+    await tester.pumpWidget(const App());
+
+    await expectLater(
+      find.byType(MainPage),
+      matchesGoldenFile('trainigs_list.png'),
+    );
+    await tester.pumpAndSettle();
+  });
+}
