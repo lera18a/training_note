1c5c1a74ee1bc3fc9bb9fc428701212806efade5
Badrakova Valeria <lera_18a@mail.ru>
2026-03-01 18:26:41 +0300

    refactor: update package structure

--- lib/date_formating_extension.dart
+++
@@ -1,5 +0,0 @@
-extension DateFormating on DateTime {
-  String formatData() {
-    return ' ${day.toString().padLeft(2, '0')}.${month.toString().padLeft(2, '0')}.$year';
-  }
-}
--- lib/models/approach.dart
+++
@@ -1,8 +0,0 @@
-import 'package:training_note/models/exercise.dart';
-
-class Approach {
-  final Exercise excercise;
-  final int repeats;
-
-  Approach({required this.excercise, required this.repeats});
-}
