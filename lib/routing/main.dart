2f73514459bbebc799668f96406ed884b38b7aed
Badrakova Valeria <lera_18a@mail.ru>
2026-03-01 18:11:10 +0300

    refactor(routing): added routing package

---
+++ lib/routing/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:training_note/routing/main_page.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: MainPage());
+  }
+}
