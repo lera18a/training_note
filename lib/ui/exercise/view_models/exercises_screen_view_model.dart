411ff1f92c980501f96264a00f501e522370d0ac
Badrakova Valeria <lera_18a@mail.ru>
2026-03-01 18:14:42 +0300

    refactor(exercise): added exercise ui package

---
+++ lib/ui/exercise/view_models/exercises_screen_view_model.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:training_note/domain/models/exercise.dart';
+
+class ExercisesViewModel extends ValueNotifier<List<Exercise>> {
+  ExercisesViewModel(List<Exercise>? exercises) : super(exercises ?? []);
+
+  void createExercise(String name) {
+    //value - сеттер оповещает автоматически всех подписчиков об изменении value (стейта)
+    final newExercise =
+        Exercise(name: name, id: DateTime.now().millisecondsSinceEpoch);
+    value = [...value, newExercise];
+  }
+
+  void updateExercise(int id, String newName) {
+    final index = value.indexWhere((e) => e.id == id);
+    // if (index == -1) return;
+    final updated = value[index].copyWith(name: newName);
+    final newList = [...value];
+    newList[index] = updated;
+    value = newList;
+  }
+
+  void deleteExercise(int id) {
+    value = value.where((e) => e.id != id).toList();
+  }
+}
+
+final ExercisesViewModel exercisesViewModel =
+    ExercisesViewModel(excercisesStub);
---
+++ lib/ui/exercise/widgets/exercise_create.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:training_note/ui/common/my_elevated.dart';
+
+class ExerciseCreate extends StatefulWidget {
+  const ExerciseCreate({super.key});
+
+  @override
+  State<ExerciseCreate> createState() => _ExerciseCreateState();
+}
+
+class _ExerciseCreateState extends State<ExerciseCreate> {
+  final TextEditingController _textController = TextEditingController();
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: TextField(
+        controller: _textController,
+        decoration: InputDecoration(hintText: 'Создать упражнение'),
+      ),
+      actions: [
+        MyElevated.cancel(
+          onPressed: () => Navigator.pop(context),
+        ),
+        MyElevated.create(
+          onPressed: () {
+            setState(() {
+              final name = _textController.text;
+              if (name.isEmpty) return;
+              Navigator.pop(context, name);
+            });
+          },
+        ),
+      ],
+    );
+  }
+}
