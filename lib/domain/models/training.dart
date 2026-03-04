1059d1ae8956672ef8bc588d7fcf679ae7893931
Badrakova Valeria <lera_18a@mail.ru>
2026-03-04 15:33:44 +0300

    refactor(models): added copyWith for training

--- lib/domain/models/training.dart
+++ lib/domain/models/training.dart
@@ -2,26 +2,45 @@ import 'package:training_note/domain/models/approach.dart';
 import 'package:training_note/domain/models/exercise.dart';
 
 class Training {
+  final int id;
   final DateTime date;
   final List<Approach> approach;
 
-  const Training({required this.date, required this.approach});
+  const Training(
+      {required this.date, required this.approach, required this.id});
+
+  Training copyWith(
+      {final int? id, final DateTime? date, final List<Approach>? approach}) {
+    return Training(
+        date: date ?? this.date,
+        approach: approach ?? this.approach,
+        id: id ?? this.id);
+  }
 }
 
 List<Training> trainings = [
-  Training(date: DateTime(2025, 04, 11), approach: [
-    Approach(excercise: excercisesStub[0], repeats: 10),
-    Approach(excercise: excercisesStub[1], repeats: 10),
-    Approach(excercise: excercisesStub[2], repeats: 10)
-  ]),
-  Training(date: DateTime(2025, 04, 11), approach: [
-    Approach(excercise: excercisesStub[0], repeats: 10),
-    Approach(excercise: excercisesStub[1], repeats: 10),
-    Approach(excercise: excercisesStub[2], repeats: 10)
-  ]),
-  Training(date: DateTime(2025, 04, 11), approach: [
-    Approach(excercise: excercisesStub[0], repeats: 10),
-    Approach(excercise: excercisesStub[1], repeats: 10),
-    Approach(excercise: excercisesStub[2], repeats: 10)
-  ]),
+  Training(
+      date: DateTime(2025, 04, 11),
+      approach: [
+        Approach(excercise: excercisesStub[0], repeats: 10),
+        Approach(excercise: excercisesStub[1], repeats: 10),
+        Approach(excercise: excercisesStub[2], repeats: 10)
+      ],
+      id: 0),
+  Training(
+      date: DateTime(2025, 04, 11),
+      approach: [
+        Approach(excercise: excercisesStub[0], repeats: 10),
+        Approach(excercise: excercisesStub[1], repeats: 10),
+        Approach(excercise: excercisesStub[2], repeats: 10)
+      ],
+      id: 1),
+  Training(
+      date: DateTime(2025, 04, 11),
+      approach: [
+        Approach(excercise: excercisesStub[0], repeats: 10),
+        Approach(excercise: excercisesStub[1], repeats: 10),
+        Approach(excercise: excercisesStub[2], repeats: 10)
+      ],
+      id: 2),
 ];
