43f6d5053f6ff4f2c86a434082c0c6beffc04ca5
JorikFat <jorikfat@gmail.com>
2026-03-20 01:46:45 +0300

    fix: golden new training

--- test/golden/trainig_create_golden_test.dart
+++ test/golden/trainig_create_golden_test.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:training_note/domain/models/exercise.dart';
 import 'package:training_note/main.dart';
+import 'package:training_note/ui/exercise/view_models/exercises_screen_view_model.dart';
+import 'package:training_note/ui/training/view_model/trainings_screen_view_model.dart';
 import 'package:training_note/ui/training/widgets/create_training.dart';
 
 void main() {
+  trainingsScreenViewModel = TrainingsScreenViewModel([]);
+  exercisesScreenViewModel = ExercisesScreenViewModel([])..value = [
+      Exercise(name: 'Отжимания', id: 1),
+      Exercise(name: 'Приседания', id: 2),
+      Exercise(name: 'Подтягивания', id: 3),
+  ];
+
   testWidgets('training details', (tester) async {
     await tester.pumpWidget(App());
 
