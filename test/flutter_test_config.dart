0316cbff0fa9164d03cba7e1d5c8c074ed7f8e9d
JorikFat <jorikfat@gmail.com>
2026-03-20 00:51:11 +0300

    add: threshold golden test

---
+++ test/flutter_test_config.dart
@@ -0,0 +1,55 @@
+import 'dart:async';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/foundation.dart';
+
+
+class LocalFileComparatorWithThreshold extends LocalFileComparator {
+  final double threshold;
+
+  LocalFileComparatorWithThreshold(super.testFile, this.threshold)
+      : assert(threshold >= 0 && threshold <= 1);
+
+  @override
+  Future<bool> compare(Uint8List imageBytes, Uri golden) async {
+    final result = await GoldenFileComparator.compareLists(
+      imageBytes,
+      await getGoldenBytes(golden),
+    );
+
+    if (!result.passed && result.diffPercent <= threshold) {
+      debugPrint(
+        'A difference of ${result.diffPercent * 100}% was found, but it is '
+        'acceptable since it is not greater than the threshold of '
+        '${threshold * 100}%',
+      );
+
+      return true;
+    }
+
+    if (!result.passed) {
+      final error = await generateFailureOutput(result, golden, basedir);
+      throw FlutterError(error);
+    }
+    return result.passed;
+  }
+}
+
+const _kGoldenTestsThreshold = 0.5 / 100;
+
+Future<void> testExecutable(FutureOr<void> Function() testMain) async {
+  if (goldenFileComparator is LocalFileComparator) {
+    final testUrl = (goldenFileComparator as LocalFileComparator).basedir;
+
+    goldenFileComparator = LocalFileComparatorWithThreshold(
+      Uri.parse('$testUrl/test.dart'),
+      _kGoldenTestsThreshold,
+    );
+  } else {
+    throw Exception(
+      'Expected `goldenFileComparator` to be of type `LocalFileComparator`, '
+      'but it is of type `${goldenFileComparator.runtimeType}`',
+    );
+  }
+
+  await testMain();
+}
